55b855020b83c7e6d5633feceadd7aeddc65073d
Victor <vcchau@gmail.com>
2019-04-16 17:48:27 -0500

    Succesfully logs data to database

--- app.py
+++ app.py
@@ -8,28 +8,33 @@ from mysql.connector import errorcode
 app = Flask(__name__)
 
 def get_db_creds():
-    db = os.environ.get("DB", None) or os.environ.get("database", None) or "reel_time_db"
-    username = os.environ.get("USER", None) or os.environ.get("username", None) or "myadmin"
-    password = os.environ.get("PASSWORD", None) or os.environ.get("password", None) or "Reeltime!"
-    hostname = os.environ.get("HOST", None) or os.environ.get("dbhost", None) or "reel-time-db.chr9q1gt6nxw.us-east-1.rds.amazonaws.com"
+    db = "reel_time_db"
+    username = "myadmin"
+    password = "Reeltime!"
+    hostname = "reel-time-db.chr9q1gt6nxw.us-east-1.rds.amazonaws.com"
     port = 3306
     return db, username, password, hostname, port
 
 def create_table():
+
+
+    print 'tryig to create table'
+    print ''
+
     # Check if table exists or not. Create and populate it only if it does not exist.
-    db, username, password, hostname = get_db_creds()
-    table_ddl = 'CREATE TABLE fishes(area TEXT, location TEXT, species TEXT, amount INT UNSIGNED NOT NULL AUTO_INCREMENTINT, PRIMARY KEY (species))'
+    db, username, password, hostname, port = get_db_creds()
+    table_ddl = 'CREATE TABLE fishes(area varchar(100), location varchar(100), species varchar(100), amount INT UNSIGNED NOT NULL, PRIMARY KEY (species))'
 
     cnx = ''
     try:
         cnx = mysql.connector.connect(user=username, password=password,
-                                      host=hostname,
+                                      host=hostname, port=port,
                                       database=db)
     except Exception as exp:
         print(exp)
         import MySQLdb
         #try:
-        cnx = MySQLdb.connect(unix_socket=hostname, user=username, passwd=password, db=db)
+        cnx = MySQLdb.connect(unix_socket=hostname, user=username, passwd=password, port=port, db=db)
         #except Exception as exp1:
         #    print(exp1)
 
@@ -38,7 +43,6 @@ def create_table():
     try:
         cur.execute(table_ddl)
         cnx.commit()
-        populate_data()
     except mysql.connector.Error as err:
 
         if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
@@ -58,17 +62,24 @@ def log_catch():
 @app.route('/log_catch', methods=['POST'])
 def add_to_db():
     print("Received request.")
-    area = request.form['area']
-    location = request.form['location']
-    species = request.form['species']
-    curr_amount = request.form['amount']
-    other = request.form['other_species']
+
+    print "********FORM*******"
+    print request.form
+    print ''
+
+    area = request.form.get('area')
+    location = request.form.get('location')
+    species = request.form.get('species')
+    curr_amount = request.form.get('amount')
+    other = request.form.get('other_species')
     if(species == 'Other'):
-    	species = request.form['other_species']
+    	species = request.form.get('other_species')
 
     # db, username, password, hostname, port = get_db_creds()
 
     print area, location, species, curr_amount, other 
+
+    create_table()
 
     cnx = ''
     try:
@@ -82,19 +93,19 @@ def add_to_db():
 
     cur = cnx.cursor()
     cur0 = cnx.cursor(buffered=True)
-    sql = ("SELECT * FROM fishes WHERE species = '%s" % (species) )
+    sql = ("SELECT * FROM fishes WHERE species = '%s'" % (species) )
     cur0.execute(sql)
     if(cur0.rowcount > 0):
         sql = ("UPDATE fishes SET amount = amount + curr_amount WHERE location = '%s' AND species = '%s'" % (location, species))
     else:
-    	sql = ("INSERT INTO fishes (area, location, species, curr_amount) VALUES (%s, %s, %s, %s)")
-    	val = (area, location, species, amount)
+    	sql = ("INSERT INTO fishes (area, location, species, amount) VALUES (%s, %s, %s, %s)")
+    	val = (area, location, species, curr_amount)
     try:
         cur.execute(sql, val)
         cnx.commit()
-        return render_template('index.html', message="Catch successfully logged!")
+        return render_template('log.html', message="Catch successfully logged!")
     except Exception as exp:
-        return render_template('index.html', message="Catch could not be logged." + str(exp))
+        return render_template('log.html', message="Catch could not be logged." + str(exp))
 
 
 if __name__ == "__main__":
